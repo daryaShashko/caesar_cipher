2eb7087d1f62ea0f4d97597a021bedb966458e67
Darya Shashko <darya_shashko@epam.com>
2020-10-05 20:00:39 +0300

    [feat]: add caesars cipher algorithm

--- src/server.js
+++ src/server.js
@@ -28,22 +28,38 @@ console.log(program.opts());
 app.listen(PORT, () =>
   console.log(`App is running on http://localhost:${PORT}`)
 );
-const genCharObj = () => {
-  let i = 'a'.charCodeAt(0);
-  const j = 'z'.charCodeAt(0);
-  let ii = 'A'.charCodeAt(0);
-  const jj = 'Z'.charCodeAt(0);
-  const b = [
-    ...Array.from({ length: jj - ii + 1 }, () => ii++),
-    ...Array.from({ length: j - i + 1 }, () => i++)
-  ];
-  return b.reduce(
-    (acc, chCode) => ({
-      ...acc,
-      [String.fromCharCode(chCode)]: chCode
-    }),
-    {}
-  );
+
+const ALPHABET_LENGTH = 26;
+const FIRST_LOWERCASE_CODE = 65;
+const LAST_LOWERCASE_CODE = 90;
+const FIRST_UPPERCASE_CODE = 97;
+const LAST_UPPERCASE_CODE = 122;
+const reg = /[a-zA-Z]+/g;
+
+const shift = 3;
+// const c = 'yzawxpeaasWQasd maskdmWpasdXZzdfae+0342 +231\n';
+const d = 'bcdzashddvZTdvg pdvngpZsdvgACcgidh+0342 +231\n';
+const isActionDecode = true;
+
+const replacer = word => {
+  const arr = [...word].map(char => {
+    const trueShift = isActionDecode
+      ? (ALPHABET_LENGTH - shift) % ALPHABET_LENGTH
+      : shift;
+    const code = char.charCodeAt(0);
+    if (code >= FIRST_LOWERCASE_CODE && code <= LAST_LOWERCASE_CODE) {
+      return (
+        ((code - FIRST_LOWERCASE_CODE + trueShift) % ALPHABET_LENGTH) +
+        FIRST_LOWERCASE_CODE
+      );
+    } else if (code >= FIRST_UPPERCASE_CODE && code <= LAST_UPPERCASE_CODE) {
+      return (
+        ((code - FIRST_UPPERCASE_CODE + trueShift) % ALPHABET_LENGTH) +
+        FIRST_UPPERCASE_CODE
+      );
+    }
+  });
+  return String.fromCharCode(...arr);
 };
 
-console.log(genCharObj());
+console.log(d.replace(reg, replacer));
